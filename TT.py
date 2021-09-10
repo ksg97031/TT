5af6d7cf297d52ab5aa66ab8c573d713395c7f30
ksg <ksg97031@gmail.com>
2021-09-11 02:18:07 +0900

    Update TT.py

--- TT.py
+++ TT.py
@@ -2,7 +2,15 @@ def taint1(msg):
   print(msg.upper())
  
 def taint2(msg):
-  print(msg.index("arg0"))
+  a = msg + "2"
+  print(a.index("arg0"))
  
 def taint3(msg):
-  print(msg.format("arg0", "arg1", "arg2"))
+  a = msg + "2"
+  b = a + "3"
+  print(b.format("arg0", "arg1", "arg2"))
+  
+if __name__ == '__main__':
+  taint1("t")
+  taint2("t")
+  taint3("t")
