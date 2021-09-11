f17da2116e6b86653a7b2c6afc9fafab1442ae7e
ksg <ksg97031@gmail.com>
2021-09-12 04:39:27 +0900

    Update TT.py

--- TT.py
+++ TT.py
@@ -9,8 +9,13 @@ def taint3(msg):
   a = msg + "2"
   b = a + "3"
   print(b.format("arg0", "arg1", "arg2"))
+ 
+def taint4(msg):
+  a = msg + "4"
+  str.format(a, "B")
   
 if __name__ == '__main__':
   taint1("t")
   taint2("t")
   taint3("t")
+  taint4("t")
